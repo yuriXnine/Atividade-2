f2ceb49c45c6e3dde11c47b0bbcbc99bedf3193c
yuriXnine <96672419+yuriXnine@users.noreply.github.com>
2023-10-10 18:24:37 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//efeito do botão voltar ao Topo
+function topo() {
+    window.scrollTo(
+        {
+            top:0,
+            left:0,
+            behavior:'smooth'
+        }
+    )
+}
+
+//Validação de Login
+function login() {
+    var logado = 0;
+    var usuario = document.getElementById('usuario').value;
+    var senha = document.getElementById('usuario').value;
+
+    if(usuario == 'admin' && senha == 'admin'){
+        window.location = 'index.html';
+        logado = 1;
+    }
+
+    if(logado == 0) {
+        alert('Acesso negado, dados incorretos');
+    }
+}
+
+//Ativar alert no botão cadastrar
+function cadastro() {
+    alert('Cadastrado com sucesso!');
+    window.location.href = 'index.html';
+}
